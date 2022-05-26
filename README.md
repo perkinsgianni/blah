588435e9b66ad1a62fde19b8da04ef39d3094456
Gianni P <perkinsgianni@gmail.com>
2022-05-26 18:21:41 -0400

    Update README.md and rename app.jsx

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## This app is deployed on Heroku: 
-## https://gp-hackathon.herokuapp.com
+<!-- ## This app is deployed on Heroku: 
+## https://gp-hackathon.herokuapp.com -->
 
 ![Project photo 1](assets/quotes.png)
 ![Project photo 2](assets/quotes2.png)
--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       symbol: '',
+      // news: [],
       quote: []
     }
 
@@ -25,6 +26,24 @@ class App extends Component {
   handleSubmit() {
     this.retrieveQuote();
   }
+
+  // componentWillMount() {
+  //   // invoke axios object
+  //   axios
+  //   // http get request to external web service
+  //   .get('https://schwab.p.rapidapi.com/news/get-market-update', {
+  //     headers: {
+  //       'X-RapidAPI-Host': 'schwab.p.rapidapi.com',
+  //       'X-RapidAPI-Key': '1ad76b53a8mshba3141588e81ad8p1deba9jsn20cf00093aa5'
+  //     },
+  //   })
+  //   .then(res => {
+  //     // first callback takes http response and returns data property
+  //     const news = res.data;
+  //   // second callback uses this.setState function to merge provided object with current object assigned to this.state
+  //     this.setState({ news });
+  //   });
+  // }
 
   retrieveQuote() {
     // invoke axios object
@@ -48,6 +67,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        {/* header */}
         <img 
           style={ { width: '100%', marginTop: '8%' }} 
           src='https://www.icas.com/__data/assets/image/0006/523428/shutterstock_394537039.jpg' 
@@ -62,21 +82,52 @@ class App extends Component {
                 value={ this.state.symbol } 
                 onChange={ this.updateSymbol } />
               <button 
-                style={ { fontSize: '10px', color: 'white', backgroundColor: 'black', float: 'right', marginTop: '4.55px', marginRight: '69.5%', borderRadius: '25px', position: 'relative' } } 
+                style={ { fontSize: '10px', color: 'white', backgroundColor: 'black', float: 'right', marginTop: '4.55px', marginRight: '69.2%', borderRadius: '25px', position: 'relative' } } 
                 onClick={ this.handleSubmit }>
                   SUBMIT
               </button>
             </div>
           </div>
-            <div style={ { height: '40px', width: '200%', backgroundColor: 'black', opacity: '0.7', marginLeft: '-10%', marginTop: '38%' } }>
+          {/* first marquee: news */}
+          {/* <div style={ { height: '40px', width: '200%', backgroundColor: 'black', opacity: '0.7', marginLeft: '-10%', marginTop: '38%' } }>
+            <marquee
+              style={ { fontSize: '18px', marginTop: '9px', color: 'white' } }
+              behavior='scroll'
+              direction='left'
+            >
+                { JSON.stringify(this.state.news['Teaser'], null, 2) }
+            </marquee>
+          </div> */}
+          {/* second marquee: quote */}
+          <div style={ { height: '40px', width: '200%', backgroundColor: 'black', opacity: '0.7', marginLeft: '-10%', marginTop: '38%' } }>
+            <marquee
+              style={ { fontSize: '18px', marginTop: '9px', color: 'white' } }
+              behavior='scroll'
+              direction='left'
+            >
+                { JSON.stringify(this.state.quote['MutualFunds'], null, 2) }
+            </marquee>
+          </div>
+
+          {/* <div style={ { height: '40px', width: '200%', backgroundColor: 'black', opacity: '0.7', marginLeft: '-10%', marginTop: '3%' } }>
+            { JSON.stringify(this.state.quote['MutualFunds'], ['MutualFunds', 'QuoteOutput', 'Change']) <= 0 ? (
               <marquee
-                style={ { fontSize: '18px', marginTop: '9px', color: 'white' } }
+                style={ { fontSize: '18px', marginTop: '9px', color: 'red' } }
                 behavior='scroll'
                 direction='left'
               >
                   { JSON.stringify(this.state.quote['MutualFunds'], null, 2) }
               </marquee>
-            </div>
+            ) : (
+              <marquee
+                style={ { fontSize: '18px', marginTop: '9px', color: 'green' } }
+                behavior='scroll'
+                direction='left'
+              >
+                  { JSON.stringify(this.state.quote['MutualFunds'], null, 2) }
+              </marquee>
+            )}
+          </div> */}
         </div>
       </div>
     );
